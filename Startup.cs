5c61507e196b630a32906a3707e63611537dedaf
DESKTOP-DRIMM2L\DELL <amruta.chouge108@gmail.com>
2021-06-25 20:04:28 +0530

    End of secction 5

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -53,6 +54,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+             // Ordering should be after routing and before EndPoints and Authentication/Authorization
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
